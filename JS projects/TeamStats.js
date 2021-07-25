4ac3e2d324d18de6fabb7b27246b8fe586d733c2
TWIrina <85954288+TWIrina@users.noreply.github.com>
2021-07-25 23:51:33 +0300

    Update TeamStats.js

--- JS projects/TeamStats.js
+++ JS projects/TeamStats.js
@@ -1,1 +1,71 @@
 
+const team = {
+    _players:[
+      {
+        firstName: 'Pablo',
+        lastName: 'Sanchez',
+        age: 11
+      },
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age:13
+      },
+      {
+        firstName: 'Garry',
+        lastName: 'Richard',
+        age: 14
+      }
+      ],
+    _games:[
+      {
+        opponet: 'Marcus',
+        teamPoints: 7,
+        opponetPoints: 9
+      },
+      {
+        opponet: 'Daniel',
+        teamPoints: 10,
+        opponetPoints: 11
+      },
+      {
+        opponet: 'Phil',
+        teamPoints: 7,
+        opponetPoints: 8
+      }
+    ],
+    get players() {
+      return this._players;
+    },
+    get games() {
+      return this._games;
+    },
+     addPlayer(firstName, lastName, age) {
+      let player = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+      };
+      this._players.push(player);
+    },
+    addGame(opponet, teamPoints, opponetPoints) {
+      let game = {
+        opponet: opponet,
+        teamPoints: teamPoints,
+        opponetPoints: opponetPoints
+      }
+      return this._games.push(game);
+    },
+  };
+  
+  team.addPlayer('Steph', 'Curry', 28);
+  team.addPlayer('Lisa','Leslie', 44);
+  team.addPlayer('Bugs','Bunny', 76);
+  
+  console.log(team._players);
+  
+  team.addGame('Marcus', 7, 9);
+  team.addGame('Daniel', 10, 11);
+  team.addGame('Phil', 7, 8);
+  
+  console.log(team._games);
