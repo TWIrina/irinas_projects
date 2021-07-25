130a44ddfa4da620b398373accdb275ecfbe39f3
TWIrina <85954288+TWIrina@users.noreply.github.com>
2021-07-26 00:07:54 +0300

    Update BuildALibrary.js

--- JS projects/BuildALibrary.js
+++ JS projects/BuildALibrary.js
@@ -1,1 +1,95 @@
 
+class Media {
+    constructor(title){
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    get title(){
+      return this._title;
+    }
+    get isCheckedOut (){
+      return this._isCheckedOut;
+    }
+    get ratings(){
+      return this._ratings;
+    }
+    set isCheckedOut(value){
+      this._isCheckedOut = value;
+    }
+     toggleCheckOutStatus(){
+      if(this._isCheckedOut === false){
+        this._isCheckedOut = true;
+      }else {
+        this._isCheckedOut = false;
+     /*alternative:
+      toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckedOut;
+  } */   
+      }
+    }
+     getAverageRating(){
+       const avgRating = this.ratings.reduce((acc,curVal)=>{
+         return Math.floor(acc + curVal / this.ratings.length);
+       })
+       return avgRating;
+     }
+    addRating(num){
+      this._ratings.push(num);
+    }
+  };
+  class Book extends Media{
+    constructor(title,author,pages){
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    get author(){
+      return this._author;
+    }
+    get pages(){
+      return this._pages;
+    }
+  };
+  class Movie extends Media{
+    constructor(title,director,runtime){
+      super(title);
+      this._director = director;
+      this._runtime = runtime;
+    }
+    get director(){
+      return this._director;
+    }
+    get runtime(){
+      return this._runtime;
+    }
+  };
+  class CD extends Media{
+    constructor(title,artist,songs){
+      super(title);
+      this._artist = artist;
+      this._songs = songs;
+    }
+    get artist(){
+      return this._artist;
+    }
+    get songs(){
+      return this._songs;
+    }
+  }
+  const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+  historyOfEverything.toggleCheckOutStatus();
+  console.log(historyOfEverything.isCheckedOut);
+  historyOfEverything.addRating(4);
+  historyOfEverything.addRating(5);
+  historyOfEverything.addRating(5);
+  console.log(historyOfEverything.ratings);
+  console.log(historyOfEverything.getAverageRating());
+  const speed = new Movie ('Speed', 'Jan de Bont', 116);
+  speed.toggleCheckOutStatus();
+  console.log(speed.isCheckedOut);
+  speed.addRating(1);
+  speed.addRating(1);
+  speed.addRating(5);
+  console.log(speed.ratings)
+  console.log(speed.getAverageRating())
